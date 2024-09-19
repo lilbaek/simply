aac8aebce6c753740131b23e3a936f384fbce881
Lilbaek <lilbaek@users.noreply.github.com>
2024-09-19 08:41:39 +0200

    Update QuerySpecImpl.java

--- simply/src/main/java/com/lilbaek/simply/QuerySpecImpl.java
+++ simply/src/main/java/com/lilbaek/simply/QuerySpecImpl.java
@@ -1,14 +1,11 @@
 package com.lilbaek.simply;
 
-import com.lilbaek.simply.sql.ResultToRecordTransformer;
-import com.lilbaek.simply.sql.StatementLogger;
+import dk.bankdata.kfa.simply.sql.ResultToRecordTransformer;
 import org.springframework.beans.BeanUtils;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.dao.TypeMismatchDataAccessException;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,10 +16,10 @@ import java.util.Optional;
 public class QuerySpecImpl implements QuerySpec {
     private final JdbcClient.StatementSpec query;
     private final String sql;
-    private final StatementLogger logger;
+    private final Logger logger;
     private final Map<String, Object> parms = new HashMap<>();
 
-    public QuerySpecImpl(final JdbcClient.StatementSpec query, final String sql, final StatementLogger logger) {
+    public QuerySpecImpl(final JdbcClient.StatementSpec query, final String sql, final Logger logger) {
         this.query = query;
         this.sql = sql;
         this.logger = logger;
@@ -44,7 +41,7 @@ public class QuerySpecImpl implements QuerySpec {
     }
 
     @Override
-    public <T> T record(final Class<T> cls) {
+    public <T> T single(final Class<T> cls) {
         final List<T> list = getList(cls);
         if (list.isEmpty()) {
             throw new EmptyResultDataAccessException(1);
@@ -56,27 +53,7 @@ public class QuerySpecImpl implements QuerySpec {
     }
 
     @Override
-    public <T> T value(final Class<T> cls) {
-        if (BeanUtils.isSimpleProperty(cls)) {
-            final RowMapper<T> rowMapper = new SingleColumnRowMapper<>(cls);
-            logger.logStatement(sql, parms);
-            return query.query(rowMapper).single();
-        }
-        throw new TypeMismatchDataAccessException(cls.getName() + " is not a simple type");
-    }
-
-    @Override
-    public <T> T valueOrNull(final Class<T> cls) {
-        if (BeanUtils.isSimpleProperty(cls)) {
-            final RowMapper<T> rowMapper = new SingleColumnRowMapper<>(cls);
-            logger.logStatement(sql, parms);
-            return query.query(rowMapper).optional().orElse(null);
-        }
-        throw new TypeMismatchDataAccessException(cls.getName() + " is not a simple type");
-    }
-
-    @Override
-    public <T> T recordOrNull(final Class<T> cls) {
+    public <T> T singleOrNull(final Class<T> cls) {
         final List<T> list = getList(cls);
         if (list.isEmpty()) {
             return null;
@@ -89,7 +66,7 @@ public class QuerySpecImpl implements QuerySpec {
 
     @Override
     public <T> Optional<T> optional(final Class<T> cls) {
-        return Optional.ofNullable(recordOrNull(cls));
+        return Optional.ofNullable(singleOrNull(cls));
     }
 
     @Override
@@ -98,19 +75,51 @@ public class QuerySpecImpl implements QuerySpec {
     }
 
     @Override
+    public int update() {
+        logger.logStatement(sql, parms);
+        return query.update();
+    }
+
+    @Override
+    public void updateSingle() {
+        Assert.state(update() == 1, "DBClient - Failed to update %s | Params: %s".formatted(sql, String.join(",", parms.entrySet().stream().map(x -> x.getKey() + " = " + x.getValue()).toList())));
+    }
+
+    @Override
+    public int delete() {
+        logger.logStatement(sql, parms);
+        return query.update();
+    }
+
+    @Override
+    public void deleteSingle() {
+        final var deleted = delete();
+        Assert.state(deleted == 1, "DBClient - Delete count was not 1. Was %d for %s | Params: %s".formatted(deleted, sql, String.join(",", parms.entrySet().stream().map(x -> x.getKey() + " = " + x.getValue()).toList())));
+    }
+
+    @Override
     public JdbcClient.StatementSpec statementSpec() {
         return query;
     }
 
     private <T> List<T> getList(final Class<T> cls) {
+        if (BeanUtils.isSimpleProperty(cls)) {
+            return query.query(cls).list();
+        }
         final var aliasToClassResultTransformer = new ResultToRecordTransformer<>(cls);
         logger.logStatement(sql, parms);
         final var queryResult = query.query();
-        final var columnNames = queryResult.rowSet().getMetaData().getColumnNames();
+        final var metaData = queryResult.rowSet().getMetaData();
+        final int columnCount = metaData.getColumnCount();
+        final var labels = new String[columnCount];
+        for (int i = 0; i < columnCount; ++i) {
+            // We use getColumnLabel as we want the alias names and not the column names
+            labels[i] = metaData.getColumnLabel(i + 1);
+        }
         final var rows = queryResult.listOfRows();
         final var result = new ArrayList<T>();
         for (final var row : rows) {
-            result.add(aliasToClassResultTransformer.map(row, columnNames));
+            result.add(aliasToClassResultTransformer.map(row, labels));
         }
         return result;
     }
